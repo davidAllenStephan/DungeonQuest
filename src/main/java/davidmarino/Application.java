ca453f29d0df52daaee9885d3163924784bb2a62
davidAllenStephan <david.marino03@hotmail.com>
2025-07-06 22:52:45 -0500

    ad

--- src/main/java/davidmarino/Application.java
+++ src/main/java/davidmarino/Application.java
@@ -1,7 +1,11 @@
 package davidmarino;
 
+import davidmarino.quest.questmodels.ArtifactCollection;
 import davidmarino.quest.questmodels.MajorCharacterCollection;
-import davidmarino.quest.questservice.MajorCharacterCollectionService;
+import davidmarino.quest.questmodels.MonsterCollection;
+import davidmarino.webscraper.ArtifactScraper;
+import davidmarino.webscraper.MajorCharacterScraper;
+import davidmarino.webscraper.MonsterCharacterScraper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -11,5 +15,14 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
         app.run(args);
+        MonsterCharacterScraper monsterCharacterScraper = new MonsterCharacterScraper();
+        MonsterCollection monsterCollection = monsterCharacterScraper.getMonsterCollection();
+        int sum = 0;
+        for (int i = 0; i < monsterCollection.getMonsterCategories().size(); i++) {
+            for (int j = 0; j < monsterCollection.getMonsterCategories().get(i).getMonsters().size(); j++) {
+                sum += 1;
+            }
+        }
+        System.out.println(sum);
     }
 }
